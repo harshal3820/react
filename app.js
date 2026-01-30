95c9a5b0c917b0118181f17c4ff324cdeb799c8d
Harshal Patil <harshal1232413@gmail.com>
2026-01-30 08:13:28 +0530

    react elements,components and JSX

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+const title = (
+  <h1 className="heading" tabIndex="5">
+    Hello from react!
+  </h1>
+);
+
+const HeadingComponent = () => (
+    <div className="container">
+        {title}
+        <h1>Hello from react component</h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent />);
